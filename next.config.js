e6cdb06738daf8b1330e5e41a56c92ba5c401427
TellaPrabhudas <dastella116@gmail.com>
2025-02-14 00:11:59 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+      domains: ['media.graphassets.com', 'ap-south-1.graphassets.com'],
+    },
+  };
+  
+  module.exports = nextConfig;
+  
